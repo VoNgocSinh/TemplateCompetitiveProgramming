dec3eeb4972dcc07dc370b4d2893acae1314dd2d
Shin6886 <91538615+Shin6886@users.noreply.github.com>
2022-10-21 09:36:13 +0700

    template.cpp

---
+++ templace.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define f1i(_n) for (int i=1;i<=_n;i++)
+#define f0i(_n) for (int i=0;i<=_n;i++)
+#define flr(_l,_r) for(int i=_l;i<=_r;i++)
+#define f1j(_2n) for (int j=1;j<=_2n;j++)
+#define f0j(_2n) for (int j=0;j<=_2n;j++)
+#define flrj(_2l,_2r) for(int j=_2l;j<=_2r;j++)
+#define pb push_back
+#define fi first
+#define se second
+#define ll long long
+#define fn "filename"
+
+int dx[8] = {0,0,1,-1,1,1,-1,-1};
+int dy[8] = {-1,1,0,0,-1,1,-1,1};
+ll INF = 1e18;
+ll mod = 1e9+7;
+
+//var
+
+
+void solve(){
+
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);cin.tie();cout.tie();
+  
+    #ifndef ONLINE_JUDGE
+        
+        freopen(fn".INP","r",stdin);
+        freopen(fn".OUT","w",stdout);
+    
+    #endif
+  //cin
+  
+  
+  
+    solve();
+}
