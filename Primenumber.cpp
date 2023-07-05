51dc2a67f35a6757fb7eccdc70dda53c136353ac
Shin6886 <91538615+Shin6886@users.noreply.github.com>
2023-07-05 22:57:45 +0700

    Update Primenumber.cpp

--- Primenumber.cpp
+++ Primenumber.cpp
@@ -1,4 +1,4 @@
-bool Prime_number(long long n){
+bool Prime(long long n){
 
 	if (!(n^2) || !(n^3))
 		return true;
@@ -6,7 +6,7 @@ bool Prime_number(long long n){
 	if (n<=1 || !(n%2) || !(n%3)) 
 		return false;
 
-	for (long long i = 5; i<= sqrt(n); i += 6)
+	for (long long i = 5; i <= sqrt(n); i += 6)
 		if (!(n%i) || !(n%(i+2)))
 			return false;
 
