a385a49f364ae880bde191b59f260473df3107fd
VoNgocSinh <91538615+VoNgocSinh@users.noreply.github.com>
2023-11-15 09:42:09 +0700

    Update Primenumber.cpp

--- Primenumber.cpp
+++ Primenumber.cpp
@@ -1,34 +1,23 @@
-bool Prime(long long n){
+bool isPrime(long long n){
 
-	if (!(n^2) || !(n^3))
-		return true;
-
-	if (n<=1 || !(n%2) || !(n%3)) 
-		return false;
-
-	for (long long i = 5; i <= sqrt(n); i += 6)
-		if (!(n%i) || !(n%(i+2)))
-			return false;
+    if (n==2 || n==3) return true;
+    if (n<=1 || n%2==0 || n%3==0) return false; 
+    for (long long i=5;i<=sqrt(n);i+=6)
+        if (n%i==0 || n%(i+2)==0) return false;
 
 	return true;
-}
-
-///checking primes
-
-void Elemental_sieve(long long n){
-
-    Es[0] = Es[1] = 1;
-
-    for (int i = 2; i<= sqrt(n); i++)
-        if (!Es[i]){
-
-            long long j = i*i;
-
-            while (j<=n)
-                Es[j] = 1,j+=i;
-
-        }
 
 }
+///checking primes - isPrime(n)=1 if n is prime, and vice versa
 
-//Elemental sieve
+bool P[N];
+void Sieve(int n) {
+
+    P[0] = P[1] = 1;
+    for (int i=2;i<=sqrt(n);i++)
+        if (!P[i])
+            for (int j=i*i;j<=n;j+=i)
+                P[j] = 1;
+
+}
+//P[i] = 0 if i is prime, and vice versa
