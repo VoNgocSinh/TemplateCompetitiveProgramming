1ea6a72955451fee86a2e312eb3ad3c0bf6f2ed3
VoNgocSinh <91538615+VoNgocSinh@users.noreply.github.com>
2023-12-21 01:08:36 +0700

    Update GenTest.cpp

--- GenTest.cpp
+++ GenTest.cpp
@@ -9,3 +9,17 @@ int Rand(int l,int r) {
     return l+abs((int)rng()%(r-l+1));
 
 }
+
+@echo off
+
+for /l %%i in (1, 1, 100000) do (
+
+    CreateTest.exe > test.inp
+    A.exe < test.inp > test.out
+    B.exe < test.inp > answer.out
+    fc test.out answer.out > 0 || echo TEST %%i [WA] && type test.inp && goto :out
+    echo TEST %%i [AC]
+
+)
+
+:out
